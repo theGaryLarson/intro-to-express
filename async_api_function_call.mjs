a26363bf781e5aaae9b277bf17fa7d290d10db8e
theGaryLarson <gary.larson316@gmail.com>
2023-02-12 13:11:45 -0800

    BC: These are my altered files where I'm trying to do the calls.

---
+++ async_api_function_call.mjs
@@ -0,0 +1,11 @@
+import { fetchOne } from 'mongo_fetch.mjs'
+
+// I'm a little confused on promises and how they are executed/resolved.
+// My goal here is just to have the function called and it log the result from
+// from within the method call. It looks like this would do double duty but compiler
+// telling me its required
+fetchOne().then(data => {
+    console.log(data);
+}).catch(error => {
+    console.error(error);
+})
