4fd864687cf5ae2c3c3fccf16e55b1245e711195
theGaryLarson <gary.larson316@gmail.com>
2023-02-15 19:37:10 -0800

    comment change in grammar

--- async_api_function_call.mjs
+++ async_api_function_call.mjs
@@ -1,7 +1,7 @@
-import { fetchOne } from 'mongo_fetch.mjs'
+import { fetchOne } from './mongo_fetch.mjs'
 
 // I'm a little confused on promises and how they are executed/resolved.
-// My goal here is just to have the function called and it log the result from
+// My goal here is just to have the function called and it log the result
 // from within the method call. It looks like this would do double duty but compiler
 // telling me its required
 fetchOne().then(data => {
